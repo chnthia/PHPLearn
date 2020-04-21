da62c26283a06a259e5ab35b8a40a7a8648b751d
chnthia <469716@qq.com>
2020-04-21 21:06:50 +0800

    Code:图形验证码类

---
+++ 09/test.php
@@ -0,0 +1,12 @@
+<?php
+
+$im = imagecreatetruecolor(100, 100);
+
+// 将背景设为红色
+$red = imagecolorallocate($im, 255, 0, 0);
+imagefill($im, 0, 0, $red);
+
+header('Content-type: image/png');
+imagepng($im);
+imagedestroy($im);
+?> 
